ba3488dd41d237a9e5265368c65830260e5aee63
seemasheikh <98578905+seemasheikh@users.noreply.github.com>
2022-12-20 21:21:05 +0500

    Add files via upload

---
+++ starbucks/assets/index.js
@@ -0,0 +1,12 @@
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything;
+
+}
+function changeCircleColor(color){
+const circle =document.querySelector('.circle');
+circle.style.background =color;
+}
+function changeHeadercolor(background){
+    const abc =document.querySelector('.abc');
+    abc.style.background =green;
+}
